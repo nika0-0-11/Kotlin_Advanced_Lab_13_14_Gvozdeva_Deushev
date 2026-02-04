bdfd19089497e988db9afc68390bd85dd4be0159
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-02-04 14:27:52 +0300

    Sets in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -13,21 +13,18 @@ val newSolarSystem = arrayOf(
     "Pluto"
 )
 fun main() {
-    val solarSystem = mutableListOf(
+    val solarSystem = mutableSetOf(
         "Mercury", "Venus", "Earth", "Mars",
         "Jupiter", "Saturn", "Uranus", "Neptune"
     )
-    /*for (planet in solarSystem) {
-        println(planet)
-    }*/
-
+    /*println(solarSystem.size)
     solarSystem.add("Pluto")
-    solarSystem.add(3,"Theia")
-    solarSystem[3] = "Future Moon"
-    println(solarSystem[3])
-    println(solarSystem[9])
-    solarSystem.removeAt(9)
-    solarSystem.remove("Future Moon")
+    println(solarSystem.size)
     println(solarSystem.contains("Pluto"))
-    println("Future Moon" in solarSystem)
+    println("Pluto" in solarSystem)
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    solarSystem.remove("Pluto")*/
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
 }
