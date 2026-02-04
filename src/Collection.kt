b6c70bb21ce7a2a736087d39cbb4eb157249900f
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-02-04 14:15:27 +0300

    Arrays in Kotlin

---
+++ src/Collection.kt
@@ -0,0 +1,27 @@
+val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+val solarSystem = rockPlanets + gasPlanets
+val newSolarSystem = arrayOf(
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+    "Pluto"
+)
+fun main() {
+    solarSystem[3] = "Little Earth"
+    println(solarSystem[3])
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
+    println(solarSystem[8])
+}
