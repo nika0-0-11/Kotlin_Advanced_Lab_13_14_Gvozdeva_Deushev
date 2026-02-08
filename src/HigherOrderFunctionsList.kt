67abd10437aadcab3776bdf9e85b26d45674cb45
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-02-08 17:51:45 +0300

    sortedBy() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -83,8 +83,14 @@ fun main() {
         println("${it.name} - $${it.price}")
     }*/
 
-    val totalPrice = cookies.fold(0.0) { total, cookie ->
+    /*val totalPrice = cookies.fold(0.0) { total, cookie ->
         total + cookie.price
     }
-    println("Total price: $${totalPrice}")
+    println("Total price: $${totalPrice}")*/
+
+    val alphabeticalMenu = cookies.sortedBy { it.name }
+    println("Меню в алфавитном порядке:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
 }
